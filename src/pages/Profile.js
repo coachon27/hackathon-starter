4506f2d0968c9dfdaf84d6ccc77002f2c15894a3
mmeacham92 <meacham.michael@rocketmail.com>
2021-02-12 16:58:11 -0600

    cleaned up code; added final styling to LoginForm component and image/kweet submit buttons

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Menu from "../components/menu/Menu";
-import Messages from "../components/messages/Messages"
+import Messages from "../components/messages/Messages";
 import { userIsAuthenticated } from "../redux/HOCs";
 
 class Profile extends React.Component {
-  
   render() {
-    let username= this.props.match?.params?.username;
+    let username = this.props.match?.params?.username;
     return (
       <div className="Profile">
         <Menu isAuthenticated={this.props.isAuthenticated} />
